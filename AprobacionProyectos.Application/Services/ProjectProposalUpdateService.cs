37af21d255f8457dc95bbf067aaa5a97104c26f1
FedericoBDiaz <fede_diaz30@hotmail.com>
2025-05-20 02:20:31 -0300

    Criterio 5 funcionando y pesistiendo. Resta criterio 6, y después revisar validaciones de todos los criterios.

---
+++ AprobacionProyectos.Application/Services/ProjectProposalUpdateService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AprobacionProyectos.Application.Interfaces;
+using AprobacionProyectos.Domain.Entities;
+using AprobacionProyectos.Infrastructure.Repositories.Interfaces;
+
+namespace AprobacionProyectos.Application.Services
+{
+    public class ProjectProposalUpdateService : IProjectProposalUpdateService
+    {
+        private readonly IProjectProposalRepository _proposalRepository;
+
+        public ProjectProposalUpdateService(IProjectProposalRepository proposalRepository)
+        {
+            _proposalRepository = proposalRepository;
+        }
+
+        public async Task<ProjectProposal?> UpdateProjectProposalAsync(Guid id, string title, string description, int duration)
+        {
+            return await _proposalRepository.UpdateAsync( id,  title,  description,  duration);
+        }
+
+    }
+}
